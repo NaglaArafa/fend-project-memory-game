9629146355c449ee45613ddfe233a2ad2f3a37c8
Naglaa Arafa <naglaa.arafa@itworx.com>
2018-01-17 16:16:02 +0200

    write comments on js file and add timer to the game

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+this is a memory game project
 
 To get started, open `js/app.js` and start building out the app's functionality
 
@@ -15,6 +15,6 @@ For specific, detailed instructions, look at the project instructions in the [Ud
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+This repository is a copy from the starter code from udacity.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
--- js/app.js
+++ js/app.js
@@ -1,30 +1,11 @@
-/*
- * Create a list that holds all of your cards
- */
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-var shuffledCards, restartGame, movesCounter = 0,
+var shuffledCards, restartGame, timer, movesCounter = 0,
     starNum = 3,
     findMatch = [],
     matchedcards = [],
-    selector = document.querySelectorAll('.stars li'),
+    starList = document.querySelectorAll('.stars li'),
     cardListContainer = document.querySelector('.deck');
+
+//Array of cards
 var cardList = ['<li class="card"><i class="fa fa-diamond"></i></li>',
     '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
     '<li class="card "><i class="fa fa-anchor"></i></li>',
@@ -44,6 +25,7 @@ var cardList = ['<li class="card"><i class="fa fa-diamond"></i></li>',
 ];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+//shuffle the list of cards
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -61,19 +43,27 @@ function shuffle(array) {
 
 //resrart function calling in the begining
 (function restart() {
+    //caling shuffle function to shuffle to list of cards and save the new list in "shuffledCards" variable
     shuffledCards = shuffle(cardList);
+    //loop over the shuffled list and add each card's HTML to the page
     shuffledCards.forEach(function(value) {
         cardListContainer.innerHTML += value;
     })
+
+    /*this function to show th whole cards at the begining for 3 seconds and hide them after that 
+     so that the user could see them once and then guess where is the matched cards like any online memory game,
+     also ecucute the function "showCard" when the user click on any card*/
     cardListContainer.childNodes.forEach(function(value) {
         setTimeout(() => value.className += " open show", 500)
         setTimeout(() => value.className = "card", 3000)
         value.addEventListener("click", ShowCard)
     })
+    startTimer();
     restartGame = restart;
-})();
+})(); // self excution function to be called at the begining
 
-//calling restart function when clicking on restart button to reset the game
+/* when calling restart button or play again after wining a game, this function will reset
+ all values to be start from the begining */
 document.querySelectorAll(".restart, .playAgain").forEach(function(element) {
     element.addEventListener("click", function() {
         cardListContainer.innerHTML = '';
@@ -83,12 +73,19 @@ document.querySelectorAll(".restart, .playAgain").forEach(function(element) {
     })
 })
 
+/*this will be excuted when the user click on any card, the card will flib and will call
+ the function "AddToList" with this card, also remove the click event from this card
+ to prevent the user from clicking to the same card and match it with itself */
 function ShowCard() {
     this.className += " open show";
     setTimeout(() => AddToList(this), 1000)
     this.removeEventListener("click", ShowCard)
 }
 
+/* after showing the card, i will add this card to a list if the list already has another card
+by checking on the length of this list, then i will check if these two cards are matched or not,
+ if true i will call "Matched", if false i will call "notMached".
+ in all cases i will clear the list to be able to match another 2 cards also call "increaseCounter" function*/
 function AddToList(element) {
     findMatch.push(element)
     if (findMatch.length > 1) {
@@ -102,19 +99,26 @@ function AddToList(element) {
     }
 }
 
+/* this function will be excuted when 2 cards matched, this will add class "match" to boyh cards,
+ and remove the click event from these cards. push these cards to "matchedcards" array and check to its length
+ to be able to know when the user finish the game */
+
+/* after finishing the game, show the wining meaage and set the number of remaining starts */
 function Matched() {
     findMatch.forEach(function(value) {
         value.className = 'card match';
         value.removeEventListener("click", ShowCard)
         matchedcards.push(value)
         if (matchedcards.length == 16) {
-            console.log("Finish")
             document.querySelector(".starsNum").innerHTML = starNum;
             document.querySelector(".winMessage").style.display = "block";
+            clearInterval(timer);
         }
     })
 }
 
+/* this function will be excuted when the cards not matched, add class not matched which i used in css for animation
+ then remove it  and add the click event handler again to card*/
 function notMached() {
     findMatch.forEach(function(value) {
         value.className += ' notmatch';
@@ -124,35 +128,57 @@ function notMached() {
     })
 }
 
+/*calling this function after matching 2 cards, to track number of moves,then write this number
+ in the html page by calling "writeMoves" function, then i saved the list items of stars in a list
+ called "starList" when the "movesCounter" reched my condition numbers then change the class name of the first 
+star then the second then the third to be empty, also decrese the "starNum" variable value */
 function increaseCounter() {
     movesCounter++;
     writeMoves(movesCounter)
     if (movesCounter == 10) {
-        selector[0].firstChild.className = "fa fa-star-o";
+        starList[0].firstChild.className = "fa fa-star-o";
         starNum--;
     }
     if (movesCounter == 15) {
-        selector[1].firstChild.className = "fa fa-star-o";
+        starList[1].firstChild.className = "fa fa-star-o";
         starNum--;
     }
     if (movesCounter == 20) {
-        selector[2].firstChild.className = "fa fa-star-o";
+        starList[2].firstChild.className = "fa fa-star-o";
         starNum--;
     }
 }
 
+/*this function called to reset out counter when starting new game like moves counter,
+ also retern the starts to be 3 again and hide the wining message */
 function resetCounter() {
     movesCounter = 0;
     writeMoves(movesCounter)
-    selector.forEach(function(element) {
+    starList.forEach(function(element) {
         element.firstChild.className = "fa fa-star";
     })
     starNum = 3;
     document.querySelector(".winMessage").style.display = "none";
 }
 
+/* set the counter value in html with the value in js*/
 function writeMoves(counter) {
     document.querySelectorAll(".moves").forEach(function(value) {
         value.innerHTML = counter;
     })
+}
+
+function startTimer() {
+    var sec = 0;
+
+    function pad(val) { return val > 9 ? val : "0" + val; }
+    timer = setInterval(function() {
+        var snd = pad(++sec % 60)
+        document.querySelectorAll("#seconds").forEach(function(value) {
+            value.innerHTML = snd;
+        })
+        document.querySelectorAll("#minutes").forEach(function(value) {
+            value.innerHTML = pad(parseInt(sec / 60, 10));
+        })
+    }, 1000);
 }
